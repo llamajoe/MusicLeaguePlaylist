c80951128b3dece1fcf417dd5513d5cc82e3f371
Steve Billingsley <sbillingsley@ITAGROUP.com>
2024-07-02 21:07:35 -0500

    added config file and table format

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const playlistId = '08r1zZNMsVQ1QexBA9rquq'; 
+const clientId = '1bf37ba023b04811bfd5b64365c03746'; 
+const clientSecret = 'a0fc8f5a15fe4f329c9dd80ea8c2dd74'; 
+
+export { clientId, clientSecret, playlistId };
